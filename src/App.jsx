de1f5dd6919ec9da851ceaf9fc1d885cf1dc230c
Md Razi Ullah <mdraziullah631@gmail.com>
2024-03-30 22:38:57 +0530

    XModal

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import XModal from './XModal'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <XModal/>
+      </div>
+    </>
+  )
+}
+
+export default App
